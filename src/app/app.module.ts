9629af6c159f4eabdbe0f94135115a017009b9fb
nydi <stefan.nydegger@gmail.com>
2018-03-15 23:07:18 +0100

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './components/home/home.component';
+import {AboutComponent} from './components/about/about.component';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
+import {RouterModule, Routes} from '@angular/router';
+import {NgxPageScrollModule} from 'ngx-page-scroll';
 
+
+const appRoutes: Routes = [
+  { path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  { path: 'dashboard',
+    component: DashboardComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    RouterModule.forRoot(appRoutes),
+    NgxPageScrollModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
